ddb5c28928ea8b10cdc4f04f34f9a76d1248a67a
BHALA2004 <balakannantvl@gmail.com>
2024-09-15 16:47:21 +0530

    Updation

--- src/com/zoho/date/DateMain.java
+++ src/com/zoho/date/DateMain.java
@@ -6,20 +6,25 @@ public class DateMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         DateImplementation dateImplementation = new DateImplementation();
-        System.out.println("Enter Date:");
+        System.out.println("Enter Date in format(DD):");
         int date = scanner.nextInt();
-        System.out.println("Enter Month:");
+        System.out.println("Enter Month in format(MM):");
         int month = scanner.nextInt();
-        System.out.println("Enter Year");
+        System.out.println("Enter Year in format(YYYY)");
         int year = scanner.nextInt();
         if(month>=1 && month<=12){
             if(date<=dateImplementation.lastDayOfMonth(date,month,year)){
+                if(year>=1000 && year<=9999){
 
         DateModel dateModel = new DateModel(date,month,year);
         System.out.println(dateModel.toString());
 
         System.out.println("Day Belongs to Date : "+dateImplementation.weekChart(dateImplementation.getDay(dateModel)));
         dateImplementation.generateFullMonthCalendar(dateModel);}
+            else {
+                    System.out.println("Invalid Year");
+                }
+            }
         else {
                 System.out.println("Date Not Present in Month");
             }
